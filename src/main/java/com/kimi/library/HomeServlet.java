5839541a7785bd820fa7c31b57e446fcbacc314f
Muskan Kumari <muski.ravi@gmail.com>
2025-11-14 12:32:44 +0530

    Initial upload of Library Managemenr System project

---
+++ src/main/java/com/kimi/library/HomeServlet.java
@@ -0,0 +1,18 @@
+package com.kimi.library;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/plain;charset=UTF-8");
+        resp.getWriter().write("LibraryMS is running on Tomcat 9!");
+    }
+}
